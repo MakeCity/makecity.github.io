e3201395148954cce4f379afa0b8a8998ad9960f
Boris Konotopskiy <boris.konotopskiy@namecheap.com>
2022-03-19 21:10:04 +0200

    feat: optimize images during production build

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,5 +1,7 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin")
+const { extendDefaultPlugins } = require("svgo");
 const { merge } = require('webpack-merge')
 
 const paths = require('./paths')
@@ -42,7 +44,42 @@ module.exports = merge(common, {
   ],
   optimization: {
     minimize: true,
-    minimizer: [new CssMinimizerPlugin(), '...'],
+    minimizer: [
+      new CssMinimizerPlugin(),
+      '...',
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            // Lossless optimization with custom option
+            // Feel free to experiment with options for better result for you
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+              // Svgo configuration here https://github.com/svg/svgo#configuration
+              [
+                "svgo",
+                {
+                  plugins: extendDefaultPlugins([
+                    {
+                      name: "removeViewBox",
+                      active: false,
+                    },
+                    {
+                      name: "addAttributesToSVGElement",
+                      params: {
+                        attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+                      },
+                    },
+                  ]),
+                },
+              ],
+            ],
+          },
+        },
+      }),
+    ],
     runtimeChunk: {
       name: 'runtime',
     },
